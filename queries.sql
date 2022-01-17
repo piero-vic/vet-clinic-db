c845e65c4227a081995c7eace928ac9675dbfce5
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-17 15:35:04 -0500

    fix(queries): Fix inconsistencies in the queries

--- queries.sql
+++ queries.sql
@@ -2,10 +2,10 @@
 SELECT * from animals WHERE name LIKE '%mon%';
 
 -- List the name of all animals born between 2016 and 2019.
-SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
-SELECT name from animals WHERE neutered IS true AND escape_attempts >= 3;
+SELECT name from animals WHERE neutered IS true AND escape_attempts <= 3;
 
 -- List date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
@@ -17,7 +17,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered IS true;
 
 -- Find all animals not named Gabumon.
-SELECT * from animals WHERE name != 'Agumon';
+SELECT * from animals WHERE name != 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
