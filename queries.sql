689a2c17d789c0dbc465c77d973bcd89bc100002
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-18 14:18:33 -0500

    fix(queries): Verify changes before and after commits

--- queries.sql
+++ queries.sql
@@ -25,16 +25,22 @@ SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 -- Transactions
 BEGIN;
 UPDATE animals SET species = 'unspecified';
+SELECT species from animals; -- verify that change was made
 ROLLBACK;
+SELECT species from animals; -- verify that change persists after commit
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT species from animals; -- verify that change was made
 COMMIT;
+SELECT species from animals; -- verify that change persists after commit
 
 BEGIN;
 DELETE FROM animals;
+SELECT * from animals; -- verify that change was made
 ROLLBACK;
+SELECT * from animals; -- verify that change persists after commit
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
@@ -42,7 +48,9 @@ SAVEPOINT SP1;
 UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO SP1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * from animals; -- verify that change was made
 COMMIT;
+SELECT * from animals; -- verify that change persists after commit
 
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
