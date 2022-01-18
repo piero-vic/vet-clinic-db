3114859aea107b4cfcd07a2121027018d7d0adcf
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-01-18 14:22:14 -0500

    fix(queries): Remove redundant comments

--- queries.sql
+++ queries.sql
@@ -25,22 +25,22 @@ SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 -- Transactions
 BEGIN;
 UPDATE animals SET species = 'unspecified';
-SELECT species from animals; -- verify that change was made
+SELECT species from animals;
 ROLLBACK;
-SELECT species from animals; -- verify that change persists after commit
+SELECT species from animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-SELECT species from animals; -- verify that change was made
+SELECT species from animals;
 COMMIT;
-SELECT species from animals; -- verify that change persists after commit
+SELECT species from animals;
 
 BEGIN;
 DELETE FROM animals;
-SELECT * from animals; -- verify that change was made
+SELECT * from animals;
 ROLLBACK;
-SELECT * from animals; -- verify that change persists after commit
+SELECT * from animals;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
@@ -48,9 +48,9 @@ SAVEPOINT SP1;
 UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO SP1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-SELECT * from animals; -- verify that change was made
+SELECT * from animals;
 COMMIT;
-SELECT * from animals; -- verify that change persists after commit
+SELECT * from animals;
 
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
